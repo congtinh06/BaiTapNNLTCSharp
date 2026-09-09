fd9a8f22528144447b7f07edcbdd42b38d190982
datthanhh206 <masonchelsea60@gmail.com>
2026-09-09 02:11:44 +0100

    Add files via upload

---
+++ Buoi1/Bai2_CVi_DtHinhTron/HinhTron.cs
@@ -0,0 +1,15 @@
+namespace BTBuoi1.BT2
+{
+    public class HinhTron
+    {
+        public double TinhDienTich(double R)
+        {
+            return Math.PI * R * R;
+        }
+
+        public double TinhChuVi(double R)
+        {
+            return 2 * Math.PI * R;
+        }
+    }
+}
