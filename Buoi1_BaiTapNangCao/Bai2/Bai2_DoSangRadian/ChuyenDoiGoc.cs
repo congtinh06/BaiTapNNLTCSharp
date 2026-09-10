d5a492e6757e76d072e007d16917e77dcff0ab69
congtinh06 <huynhcongtinh2006193@gmail.com>
2026-09-10 10:04:54 +0700

    Buổi 1 - Bài Tập Nâng Cao

---
+++ Buoi1_BaiTapNangCao/Bai2/Bai2_DoSangRadian/ChuyenDoiGoc.cs
@@ -0,0 +1,11 @@
+namespace DoSangRadian;
+
+public class ChuyenDoiGoc
+{
+    // Cong thuc: radian = do x PI / 180
+    // Tra ve gia tri double (khong in truc tiep) de co the unit test duoc
+    public static double DoiSangRadian(double doGoc)
+    {
+        return doGoc * Math.PI / 180;
+    }
+}
---
+++ Buoi1_BaiTapNangCao/Bai3/Bai3_HamSo1/HamSo1.cs
@@ -0,0 +1,30 @@
+﻿// Khai bao thu vien
+using System;
+using HamSo1;
+
+namespace HamSo1.App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Khai bao bien
+            double x, y1, y2;
+
+            // Nhap du lieu
+            Console.Write("Nhap so thuc x: ");
+            x = double.Parse(Console.ReadLine()!);
+
+            // Xu ly: goi ham tinh y1, y2 tu thu vien HamSo1
+            y1 = TinhHamSo.TinhY1(x);
+            y2 = TinhHamSo.TinhY2(x);
+
+            // Xuat du lieu, lam tron 2 so le thap phan
+            Console.WriteLine("y1 = {0:0.00}", y1);
+            Console.WriteLine("y2 = {0:0.00}", y2);
+
+            // Dung chuong trinh cho nhap phim
+            Console.Read();
+        }
+    }
+}
