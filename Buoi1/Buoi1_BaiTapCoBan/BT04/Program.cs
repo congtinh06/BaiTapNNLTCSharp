1c969e84c2a6a91112bd550aa68f451c0a85e73b
datthanhh206 <masonchelsea60@gmail.com>
2026-09-14 11:17:17 +0700

    Remove old folders

--- Buoi1/Buoi1_BaiTapCoBan/BT04/Program.cs
+++
@@ -1,9 +0,0 @@
-using NNLTCSharp.Buoi01;
-
-Console.Write("Nhap vao tong so giay: ");
-
-long tongGiay = long.Parse(Console.ReadLine()!);
-
-string ketQua = ThoiGian.DoiThoiGian(tongGiay);
-
-Console.WriteLine($"{tongGiay} giay co dang {ketQua}");
--- Buoi2/Buoi02_BaiTapNangCao/BT07_DienTichToDam/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using NNLTCSharp.Buoi02;
-
-Console.Write("Nhap a: ");
-double a = double.Parse(Console.ReadLine()!);
-
-double ketQua = DienTichToDam.TinhDienTich(a);
-
-Console.WriteLine($"Dien tich to dam = {ketQua:F2}");
